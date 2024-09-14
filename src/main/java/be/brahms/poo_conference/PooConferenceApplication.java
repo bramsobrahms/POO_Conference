cfcd026ae980874f04d8dbb8684157826fafb1e2
Brahms <brahmsisme@gmail.com>
2024-09-15 00:46:49 +0200

    POO Java - Conference with Junit and jackson Dataformat

---
+++ src/main/java/be/brahms/poo_conference/PooConferenceApplication.java
@@ -0,0 +1,13 @@
+package be.brahms.poo_conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooConferenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/brahms/poo_conference/PooConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.brahms.poo_conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
